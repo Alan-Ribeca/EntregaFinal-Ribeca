080c3b269c619c81d4d7918ac3902518dbbe7eac
Alan Ribeca <alanribeca2003@gmail.com>
2024-03-11 18:56:23 -0300

    agregando la funcion de cierre a los botones del nav

--- src/components/botonesNavbar/BotonesNavbar.jsx
+++ src/components/botonesNavbar/BotonesNavbar.jsx
@@ -1,42 +1,85 @@
 import "./botonesNavbar.scss";
+import { useState, useEffect } from "react";
 import { CartWidget } from "../cardtWidget/CartWidget";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const BotonesNavbar = () => {
+  const [dispositivosAbierto, setDispositivosAbierto] = useState(false);
+  const [perifericosAbierto, setPerifericosAbierto] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setDispositivosAbierto(false);
+    setPerifericosAbierto(false);
+  }, [location]);
+
+  const toggleDispositivos = () => {
+    setDispositivosAbierto(!dispositivosAbierto);
+  };
+
+  const togglePerifericos = () => {
+    setPerifericosAbierto(!perifericosAbierto);
+  };
+
+  const closeDetails = () => {
+    setDispositivosAbierto(false);
+    setPerifericosAbierto(false);
+  };
+
   return (
-    <>
-      <div className="botonesNavbar">
-        <Link to={"/"}>
-          <button className="botonNav"> Inicio </button>
-        </Link>
-        <details className="botonNav">
-          <summary>Dispositivos</summary>
-          <ul>
-            <Link to={"/category/Pc"}>
-              <li className="liBotones">Pc</li>
-            </Link>
-            <Link to={"/category/Netbook"}>
-              <li className="liBotones">Netbook</li>
-            </Link>
-            <Link to={"/category/Celular"}>
-              <li className="liBotones">Celulares</li>
-            </Link>
-          </ul>
-        </details>
-        <details className="botonNav">
-          <summary>Perifericos</summary>
+    <div className="botonesNavbar">
+      <Link to={"/"}>
+        <button className="botonNav">Inicio</button>
+      </Link>
+      <details
+        className="botonNav"
+        open={dispositivosAbierto}
+        onClick={toggleDispositivos}
+      >
+        <summary>Dispositivos</summary>
+        <ul>
+          <Link to={"/category/Pc"}>
+            <li className="liBotones" onClick={closeDetails}>
+              Pc
+            </li>
+          </Link>
+          <Link to={"/category/Netbook"}>
+            <li className="liBotones" onClick={closeDetails}>
+              Netbook
+            </li>
+          </Link>
+          <Link to={"/category/Celular"}>
+            <li className="liBotones" onClick={closeDetails}>
+              Celulares
+            </li>
+          </Link>
+        </ul>
+      </details>
+      <details
+        className="botonNav"
+        open={perifericosAbierto}
+        onClick={togglePerifericos}
+      >
+        <summary>Periféricos</summary>
+        <ul>
           <Link to={"/category/Monitor"}>
-            <li className="liBotones">Monitor</li>
+            <li className="liBotones" onClick={closeDetails}>
+              Monitor
+            </li>
           </Link>
           <Link to={"/category/accesorios"}>
-            <li className="liBotones">Auriculares</li>
+            <li className="liBotones" onClick={closeDetails}>
+              Auriculares
+            </li>
           </Link>
           <Link to={"/category/Componente"}>
-            <li className="liBotones">Componentes</li>
+            <li className="liBotones" onClick={closeDetails}>
+              Componentes
+            </li>
           </Link>
-        </details>
-        <CartWidget />
-      </div>
-    </>
+        </ul>
+      </details>
+      <CartWidget />
+    </div>
   );
 };
