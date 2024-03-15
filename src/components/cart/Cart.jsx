19cd792a5a87ae2cc6e7942b18218cb98b362849
Alan Ribeca <alanribeca2003@gmail.com>
2024-03-15 17:36:14 -0300

    carrito responsive

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -8,12 +8,12 @@ export const Cart = () => {
   const { carrito, emptyCart } = useCarritoContext();
 
   const formattedPrice = (price) => {
-    return price.toLocaleString('es-ES');
-  }
+    return price.toLocaleString("es-ES");
+  };
 
   const calculateTotalPrice = () => {
     let totalPrice = 0;
-    carrito.forEach(product => {
+    carrito.forEach((product) => {
       totalPrice += product.quantity * product.price;
     });
     return totalPrice;
@@ -36,27 +36,34 @@ export const Cart = () => {
             <div className="itemProductos">
               {<ItemList products={carrito} plantilla={ItemCart} />}
             </div>
-            <div className="resumenCompra">
-              <h1>Resumen de compra</h1>
-              <hr className="hrCarrito" />
-              <p className="envio">
-                Envio <strong className="gratis">Gratis</strong>
-              </p>
-              <p className="prueba">
-                Total <strong className="totalPrice">${formattedPrice(calculateTotalPrice())}</strong>
-              </p>
-              <div className="botonResumen">
-                <button onClick={emptyCart} className="vaciarCarrito">
-                  Vaciar carrito
-                </button>
-                <Link to={"/"}>
-                  <button className="seguirComprando">Seguir comprando</button>
+            <section className="resumen">
+              <div className="resumenCompra">
+                <h1>Resumen de compra</h1>
+                <hr className="hrCarrito" />
+                <p className="envio">
+                  Envio <strong className="gratis">Gratis</strong>
+                </p>
+                <p className="prueba">
+                  Total{" "}
+                  <strong className="totalPrice">
+                    ${formattedPrice(calculateTotalPrice())}
+                  </strong>
+                </p>
+                <div className="botonResumen">
+                  <button onClick={emptyCart} className="vaciarCarrito">
+                    Vaciar carrito
+                  </button>
+                  <Link to={"/"}>
+                    <button className="seguirComprando">
+                      Seguir comprando
+                    </button>
+                  </Link>
+                </div>
+                <Link to={"/checkout"}>
+                  <button className="finalizarCompra">Finalizar compra</button>
                 </Link>
               </div>
-              <Link to={"/checkout"}>
-                <button className="finalizarCompra">Finalizar compra</button>
-              </Link>
-            </div>
+            </section>
           </section>
         </>
       )}
